dd1c4b286f246765c564794570cc8725e58c74aa
amit <amitoosh.mor@gmail.com>
2021-02-13 08:01:11 +0200

    chaninig

--- object1/multipy.js
+++ object1/multipy.js
@@ -6,13 +6,3 @@ function multiplyNumeric(obj) {
        }
     }    
 }
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
-
-console.log(menu);  
-multiplyNumeric(menu);
-console.log(menu);
-  
